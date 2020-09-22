2d481c7d7f659db7ba46f4253fc832abf373a1ac
Enderson Lúcio Garcia <endersonluciog@gmail.com>
2020-09-21 23:56:42 -0300

    all

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ class App {
   middlewares() {
     this.server.use(
       cors({
-        origin: ['http://localhost:3000', process.env.APP_URL],
+        origin: '*',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
         allowedHeaders: '*',
         exposedHeaders: 'x-total-count',
